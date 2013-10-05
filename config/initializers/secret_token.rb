46eb819adae0b3e16069d2429eacd18f095176cb
MyTicketToRide <MyHomeFolder@MyTicketToRides-MacBook-Pro.local>
2013-10-05 10:15:54 -0400

    Initail Commit for the Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '927aaa77b767661bf998ef28d35adc0fb9648152824fee2e29d09d2e7934a975f56a68c20e46e0e133b65c98e5698ed0871db1a472a0a443109e4b8422124873'
